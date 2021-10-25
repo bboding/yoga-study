1f8b3abb96af5e3b99f5a3d72801907973db7ffd
bboding <dingbboding@gmail.com>
2021-10-25 21:56:51 +0900

    클릭으로 변경

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,12 +2,14 @@
   <div>
     <div>
       <h3>테스트</h3>
-      <a href="/test-asana-name">아쉬탕가 아사나 이름 외우기</a>
+      <button @click="$router.push('/test-asana-name')">
+        아사나 이름 외우기
+      </button>
       <!-- <a href="/asana-sequence">아사나 순서 외우기</a> -->
     </div>
     <div>
       <h3>스터디</h3>
-      <button @click="onClickAsana">아쉬탕가 아사나 공부</button>
+      <button @click="$router.push('/asana')">아쉬탕가 요가 아사나</button>
     </div>
   </div>
 </template>
@@ -18,11 +20,6 @@ export default {
     return {
       title: "yoga study",
     };
-  },
-  methods: {
-    onClickAsana() {
-      this.$router.push("/asana");
-    },
   },
 };
 </script>
